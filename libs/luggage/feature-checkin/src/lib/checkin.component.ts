b1b9859c58106719fbbd67faeafd842998f01b4b
mikezks <23002661+mikezks@users.noreply.github.com>
2023-02-17 11:39:58 +0100

    feat: ddd luggage as template tag

---
+++ libs/luggage/feature-checkin/src/lib/checkin.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckinFacade } from '@flight-demo/luggage/domain';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'luggage-checkin',
+  templateUrl: './checkin.component.html',
+  styleUrls: ['./checkin.component.scss'],
+  // encapsulation: ViewEncapsulation.None
+})
+export class CheckinComponent implements OnInit {
+  luggageList$ = this.checkinFacade.luggageList$;
+
+  constructor(private checkinFacade: CheckinFacade) {}
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load(): void {
+    this.checkinFacade.load();
+  }
+}
